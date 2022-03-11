5b14fcd61382db4827e8d9bffb5b72c538261c93
dannehtran <dannytran141@gmail.com>
2022-03-11 01:22:21 -0800

    fixed format output

--- Q1_Rate_Limiting/rate_limit.py
+++ Q1_Rate_Limiting/rate_limit.py
@@ -3,9 +3,6 @@
 from clfparser import CLFParser
 from datetime import datetime
 
-# output as a CSV and sort by timestamp
-# Output unban time once banned then continue with iteration
-
 LIMITER_DICT = {}
 
 def RateLimiter(log_tuple):
@@ -17,18 +14,36 @@ def RateLimiter(log_tuple):
                 if LIMITER_DICT[log_tuple[0]]["login_first_seen"] == 0 and LIMITER_DICT[log_tuple[0]]["login_last_seen"] == 0:
                     LIMITER_DICT[log_tuple[0]]["login_first_seen"] = log_tuple[1]
                     LIMITER_DICT[log_tuple[0]]["login_last_seen"] = LIMITER_DICT[log_tuple[0]]["login_first_seen"]
-                else:
-                    LIMITER_DICT[log_tuple[0]]["login_last_seen"] = log_tuple[1]
             CheckRateLimiter(log_tuple)
         elif LIMITER_DICT[log_tuple[0]]["ban_status"] == "BAN":
             CheckBanTimer(log_tuple)
             
     else:
-        count = 1
         if "/login" in log_tuple[2]:
-            LIMITER_DICT[log_tuple[0]] = {"count":count,"first_seen": log_tuple[1],"ban_status":"None", "ban_time":0, "last_seen":log_tuple[1],"login_count":count, "login_first_seen":log_tuple[1], "login_last_seen":log_tuple[1]}
+            LIMITER_DICT[log_tuple[0]] = {"count":0, "counter_20":0, "first_seen": log_tuple[1],"ban_status":"None", "ban_time":0, "last_seen":log_tuple[1],"login_count":0, "login_first_seen":log_tuple[1], "login_last_seen":log_tuple[1]}
         else:
-            LIMITER_DICT[log_tuple[0]] = {"count":count,"first_seen": log_tuple[1],"ban_status":"None", "ban_time":0, "last_seen":log_tuple[1],"login_count":0, "login_first_seen":0, "login_last_seen":0}
+            LIMITER_DICT[log_tuple[0]] = {"count":0, "counter_20":0, "first_seen": log_tuple[1],"ban_status":"None", "ban_time":0, "last_seen":log_tuple[1],"login_count":0, "login_first_seen":0, "login_last_seen":0}
+
+def CheckLoginLimiter(log_tuple):
+    ip = log_tuple[0]
+    request_header = log_tuple[2]
+    login_count = LIMITER_DICT[ip]["login_count"]
+    login_first = LIMITER_DICT[ip]["login_first_seen"]
+    login_last = LIMITER_DICT[ip]["login_last_seen"]
+    latest_request = datetime.timestamp(log_tuple[1])
+
+    if login_count >= 20:
+        if "/login" in request_header:
+            time_delta_login = login_last - login_first
+            if time_delta_login.total_seconds() <= 600:
+                LIMITER_DICT[ip]["ban_time"] = latest_request + 7200
+                LIMITER_DICT[ip]["ban_status"] = "BAN"
+                print("{},{},{}".format(int(latest_request), "BAN", ip))
+                print("{},{},{}".format(int(LIMITER_DICT[ip]["ban_time"]), "UNBAN", ip))
+            else:
+                LIMITER_DICT[ip]["login_first_seen"] = log_tuple[1]
+                LIMITER_DICT[ip]["login_last_seen"] = LIMITER_DICT[ip]["login_first_seen"]
+                LIMITER_DICT[ip]["login_count"] = 1
 
 def CheckRateLimiter(log_tuple):
     ip = log_tuple[0]
@@ -40,54 +55,63 @@ def CheckRateLimiter(log_tuple):
     login_first = LIMITER_DICT[ip]["login_first_seen"]
     login_last = LIMITER_DICT[ip]["login_last_seen"]
     latest_request = datetime.timestamp(log_tuple[1])
-    unix_first_seen = datetime.timestamp(ip_first)
     time_delta = ip_last - ip_first
-    time_delta_unix = latest_request - unix_first_seen
 
-    if ip_count >= 20:
-        if login_count >= 20:
-            if "/login" in request_header:
-                time_delta_login = login_last - login_first
-                if time_delta_login.total_seconds() < 600:
-                    LIMITER_DICT[ip]["ban_time"] = latest_request + 7200
-                    LIMITER_DICT[ip]["ban_status"] = "BAN"
-                    print("{},{},{}".format(int(LIMITER_DICT[ip]["ban_time"]), "BAN", ip))
-                    print("{},{},{}".format(int(LIMITER_DICT[ip]["ban_time"]), "UNBAN", ip))
-                else:
-                    LIMITER_DICT[ip]["login_first_seen"] = log_tuple[1]
-                    LIMITER_DICT[ip]["login_last_seen"] = LIMITER_DICT[ip]["login_first_seen"]
-                    LIMITER_DICT[ip]["login_count"] = 1
-
-    if ip_count >= 40 and ip_count < 100:
-        if time_delta.total_seconds() < 60:
-            LIMITER_DICT[ip]["ban_time"] = latest_request + 600
-            LIMITER_DICT[ip]["ban_status"] = "BAN"
-            print("{},{},{}".format(int(LIMITER_DICT[ip]["ban_time"]), "BAN", ip))
-            print("{},{},{}".format(int(LIMITER_DICT[ip]["ban_time"]), "UNBAN", ip))
+    if ip_count == 20:
+        LIMITER_DICT[ip]["counter_20"] = LIMITER_DICT[ip]["counter_20"] + 1
+        LIMITER_DICT[ip]["count"] = 0
+        
+    if LIMITER_DICT[ip]["counter_20"] % 2 == 0 and LIMITER_DICT[ip]["counter_20"] != 0 and ip_count == 0:
+        if "/login" in request_header and login_count < 20:
+            LIMITER_DICT[ip]["last_seen"] = log_tuple[1]                 
+            LIMITER_DICT[ip]["login_last_seen"] = LIMITER_DICT[ip]["last_seen"]
+            LIMITER_DICT[ip]["login_count"] = LIMITER_DICT[ip]["login_count"] + 1
+            LIMITER_DICT[ip]["count"] = LIMITER_DICT[ip]["count"] + 1 
         else:
-            LIMITER_DICT[ip]["first_seen"] = log_tuple[1]
-            LIMITER_DICT[ip]["last_seen"] = LIMITER_DICT[ip]["first_seen"]
-            LIMITER_DICT[ip]["count"] = 1
+            if time_delta.total_seconds() <= 60:
+                LIMITER_DICT[ip]["ban_time"] = latest_request + 600
+                LIMITER_DICT[ip]["ban_status"] = "BAN"
+                print(int(latest_request), "BAN", ip)
+                print(int(LIMITER_DICT[ip]["ban_time"]), "UNBAN", ip)
+            if time_delta.total_seconds() > 60: 
+                if login_count < 20:
+                    if LIMITER_DICT[ip]["counter_20"] == 2 and ip_count == 0:
+                        LIMITER_DICT[ip]["first_100_seen"] = LIMITER_DICT[ip]["first_seen"]                  
+                    LIMITER_DICT[ip]["last_seen"] = log_tuple[1]
+                    LIMITER_DICT[ip]["first_seen"] = LIMITER_DICT[ip]["last_seen"]
+                    LIMITER_DICT[ip]["count"] = LIMITER_DICT[ip]["count"] + 1  
+                if login_count >= 20:
+                    CheckLoginLimiter(log_tuple)
 
-    if ip_count >= 100:
-        if time_delta.total_seconds() < 600:
+    if LIMITER_DICT[ip]["counter_20"] % 5 == 0 and LIMITER_DICT[ip]["counter_20"] != 0 and ip_count == 0:
+        if "first_100_seen" in LIMITER_DICT[ip]:
+            time_delta = log_tuple[1] - LIMITER_DICT[ip]["first_100_seen"]
+        else:
+            time_delta = ip_last - ip_first
+        if time_delta.total_seconds() <= 600:
             LIMITER_DICT[ip]["ban_time"] = latest_request + 3600
             LIMITER_DICT[ip]["ban_status"] = "BAN"
-            print("{},{},{}".format(int(LIMITER_DICT[ip]["ban_time"]), "BAN", ip))
-            print("{},{},{}".format(int(LIMITER_DICT[ip]["ban_time"]), "UNBAN", ip))
+            if "first_100_seen" in LIMITER_DICT[ip]:
+                print(int(latest_request), "BAN", ip)
+            else:
+                print(int(LIMITER_DICT[ip]["ban_time"]), "UNBAN", ip)
+        if time_delta.total_seconds() > 600:
+            if login_count < 20:
+                LIMITER_DICT[ip]["last_seen"] = log_tuple[1]
+                LIMITER_DICT[ip]["first_100_seen"] = LIMITER_DICT[ip]["last_seen"]
+                LIMITER_DICT[ip]["count"] = LIMITER_DICT[ip]["count"] + 1
+            if login_count >= 20:
+                CheckLoginLimiter(log_tuple) 
+    
+    if ip_count < 20 and login_count < 20:
+        if "/login" in request_header:
+            LIMITER_DICT[ip]["last_seen"] = log_tuple[1]                 
+            LIMITER_DICT[ip]["login_last_seen"] = LIMITER_DICT[ip]["last_seen"]
+            LIMITER_DICT[ip]["login_count"] = LIMITER_DICT[ip]["login_count"] + 1
+            LIMITER_DICT[ip]["count"] = LIMITER_DICT[ip]["count"] + 1
         else:
-            LIMITER_DICT[ip]["first_seen"] = log_tuple[1]
-            LIMITER_DICT[ip]["last_seen"] = LIMITER_DICT[ip]["first_seen"]
-            LIMITER_DICT[ip]["count"] = 1
-
-    elif "/login" in request_header:
-        LIMITER_DICT[ip]["last_seen"] = log_tuple[1]
-        LIMITER_DICT[ip]["login_last_seen"] = LIMITER_DICT[ip]["last_seen"]
-        LIMITER_DICT[ip]["login_count"] = LIMITER_DICT[ip]["login_count"] + 1
-        LIMITER_DICT[ip]["count"] = LIMITER_DICT[ip]["count"] + 1
-    else:
-        LIMITER_DICT[ip]["last_seen"] = log_tuple[1]
-        LIMITER_DICT[ip]["count"] = LIMITER_DICT[ip]["count"] + 1
+            LIMITER_DICT[ip]["last_seen"] = log_tuple[1]
+            LIMITER_DICT[ip]["count"] = LIMITER_DICT[ip]["count"] + 1
         
 def CheckBanTimer(log_tuple):
     time_stamp_request = datetime.timestamp(log_tuple[1])
@@ -96,10 +120,7 @@ def CheckBanTimer(log_tuple):
     time_delta = time_stamp_request - ban_timer
 
     if time_delta >= 0:
-        if "/login" in log_tuple[2]:
-            LIMITER_DICT[log_tuple[0]] = {"count":1,"first_seen": log_tuple[1],"ban_status":"None", "ban_time":0, "last_seen":log_tuple[1],"login_count":1, "login_first_seen":log_tuple[1], "login_last_seen":log_tuple[1]}
-        else:
-            LIMITER_DICT[log_tuple[0]] = {"count":1,"first_seen": log_tuple[1],"ban_status":"None", "ban_time":0, "last_seen":log_tuple[1],"login_count":1, "login_first_seen":0, "login_last_seen":0}
+        LIMITER_DICT.pop(log_tuple[0])
         RateLimiter(log_tuple)
     else:
         pass
